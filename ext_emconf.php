e9229ad128707731c4b37d9eb1b3a8294dc245db
Thomas Hohn <tho@systime.dk>
2019-04-23 10:12:21 +0200

    [TASK] Raised php and typo3 depencies to 7.3.99 and 8.5.99-9.99.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,8 +23,8 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'php' => '7.0.0-7.1.99',
-      'typo3' => '7.6.13-8.5.99',
+      'php' => '7.0.0-7.3.99',
+      'typo3' => '8.5.99-9.99.99',
     ),
     'conflicts' =>
     array (
