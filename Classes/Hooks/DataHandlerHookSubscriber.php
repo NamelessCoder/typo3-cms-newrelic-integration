49f244254e1c632abe54a852f13f2262d87d532b
Dennis Prinse <dennis.prinse@maxserv.com>
2020-12-03 11:34:16 +0100

    Initial v10 compatible version update

--- Classes/Hooks/DataHandlerHookSubscriber.php
+++ Classes/Hooks/DataHandlerHookSubscriber.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace NamelessCoder\NewrelicIntegration\Hooks;
 
 use TYPO3\CMS\Core\DataHandling\DataHandler;
--- ext_emconf.php
+++ ext_emconf.php
@@ -23,8 +23,8 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'php' => '7.0.0-7.3.99',
-      'typo3' => '8.5.99-9.99.99',
+      'php' => '7.0.0-7.4.99',
+      'typo3' => '8.5.99-10.99.99',
     ),
     'conflicts' =>
     array (
