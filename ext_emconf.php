c33fba6810b3fbb22ab2c7b70886b629b3517e11
Claus Due <claus@namelesscoder.net>
2021-02-03 11:04:34 +0100

    Adjust dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,35 +1,40 @@
 <?php
-
-$EM_CONF['newrelic_integration'] = [
-    'title' => 'Newrelic Integration',
-    'description' => 'A collection of integrations for Newrelic monitoring',
-    'category' => 'misc',
-    'author' => 'Claus Due / NamelessCoder',
-    'author_email' => 'claus@namelesscoder.net',
-    'author_company' => '',
-    'shy' => '',
-    'dependencies' => '',
-    'conflicts' => '',
-    'priority' => '',
-    'module' => '',
-    'state' => 'beta',
-    'internal' => '',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
-    'clearCacheOnLoad' => 0,
-    'lockType' => '',
-    'version' => '1.2.0',
-    'constraints' =>
-        [
-            'depends' =>
-                [
-                    'php' => '7.0.0-7.3.99',
-                    'typo3' => '8.5.99-9.99.99',
-                ],
-            'conflicts' => [],
-            'suggests' => [],
-        ],
-    'suggests' => [],
-    '_md5_values_when_last_written' => '',
-];
+$EM_CONF['newrelic_integration'] = array (
+  'title' => 'Newrelic Integration',
+  'description' => 'A collection of integrations for Newrelic monitoring',
+  'category' => 'misc',
+  'author' => 'Claus Due / NamelessCoder',
+  'author_email' => 'claus@namelesscoder.net',
+  'author_company' => '',
+  'shy' => '',
+  'dependencies' => '',
+  'conflicts' => '',
+  'priority' => '',
+  'module' => '',
+  'state' => 'beta',
+  'internal' => '',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'modify_tables' => '',
+  'clearCacheOnLoad' => 0,
+  'lockType' => '',
+  'version' => '1.2.0',
+  'constraints' =>
+  array (
+    'depends' =>
+    array (
+      'php' => '7.0.0-7.4.99',
+      'typo3' => '8.7.0-10.4.99',
+    ),
+    'conflicts' =>
+    array (
+    ),
+    'suggests' =>
+    array (
+    ),
+  ),
+  'suggests' =>
+  array (
+  ),
+  '_md5_values_when_last_written' => '',
+);
