324fb4d842671818912877d885d9cd515325b38a
thomashohn <tho@systime.dk>
2023-07-25 13:27:25 +0200

    [FEATURE] Declare support for PHP 8.2
    Close: #13

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,40 +1,33 @@
 <?php
-$EM_CONF['newrelic_integration'] = array (
-  'title' => 'Newrelic Integration',
-  'description' => 'A collection of integrations for Newrelic monitoring',
-  'category' => 'misc',
-  'author' => 'Claus Due / NamelessCoder',
-  'author_email' => 'claus@namelesscoder.net',
-  'author_company' => '',
-  'shy' => '',
-  'dependencies' => '',
-  'conflicts' => '',
-  'priority' => '',
-  'module' => '',
-  'state' => 'beta',
-  'internal' => '',
-  'uploadfolder' => 0,
-  'createDirs' => '',
-  'modify_tables' => '',
-  'clearCacheOnLoad' => 0,
-  'lockType' => '',
-  'version' => '1.4.1',
-  'constraints' =>
-  array (
-    'depends' =>
-    array (
-      'php' => '7.0.0-8.1.99',
-      'typo3' => '8.7.0-11.5.99',
-    ),
-    'conflicts' =>
-    array (
-    ),
-    'suggests' =>
-    array (
-    ),
-  ),
-  'suggests' =>
-  array (
-  ),
-  '_md5_values_when_last_written' => '',
-);
+
+$EM_CONF['newrelic_integration'] = [
+    'title' => 'Newrelic Integration',
+    'description' => 'A collection of integrations for Newrelic monitoring',
+    'category' => 'misc',
+    'author' => 'Claus Due / NamelessCoder',
+    'author_email' => 'claus@namelesscoder.net',
+    'author_company' => '',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '1.4.2',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.0.0-8.2.99',
+            'typo3' => '8.7.0-11.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'suggests' => [],
+    '_md5_values_when_last_written' => '',
+];
